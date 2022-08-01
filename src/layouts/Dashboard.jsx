637da69454f39a966652f25a5d75aed17ef872a4
GurbanAlizada <88265011+GurbanAlizada@users.noreply.github.com>
2022-08-01 17:47:36 +0400

    commit

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProductList from '../pages/ProductList'
+import Categories from './Categories'
+import Navi from './Navi'
+
+export default function Dashboard() {
+  return (
+    <div>
+
+      <Navi/>
+      <Categories/>
+      <ProductList/>
+
+    </div>
+  )
+}
+
